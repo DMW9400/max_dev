298b3a42b8e64b05c8adfd0d76fd7f1c986be4fb
DMW9400 <dmw9400@gmail.com>
2025-12-12 23:13:56 -0500

    ctrlGen done!

--- ctrlGen/scriptUI_generator.js
+++ ctrlGen/scriptUI_generator.js
@@ -1,26 +1,33 @@
 autowatch = 1;
 inlets = 1;
+outlets = 1;
 
 var patcher = this.patcher;
 
 // Grid layout settings
 var GRID_START_X = 50;
-var GRID_START_Y = 100;
+var GRID_START_Y = 185;
 var DIAL_WIDTH = 50;
 var DIAL_HEIGHT = 50;
 var COMMENT_HEIGHT = 20;
 var SCALE_WIDTH = 90;
 var SCALE_HEIGHT = 20;
 var REMOTE_HEIGHT = 22;
-var GROUP_SPACING = 60;
+var GROUP_SPACING_X = 47;
+var GROUP_SPACING_Y = 40;
 var COLUMNS = 10;
 
 // Calculate total cell size (comment + dial + scale + remote + spacing)
-var CELL_WIDTH = SCALE_WIDTH + GROUP_SPACING;
-var CELL_HEIGHT = COMMENT_HEIGHT + DIAL_HEIGHT + SCALE_HEIGHT + REMOTE_HEIGHT + GROUP_SPACING;
+var CELL_WIDTH = SCALE_WIDTH + GROUP_SPACING_X;
+var CELL_HEIGHT = COMMENT_HEIGHT + DIAL_HEIGHT + SCALE_HEIGHT + REMOTE_HEIGHT + GROUP_SPACING_Y;
 
 // Store created objects for potential cleanup
 var createdObjects = [];
+
+// Store devices and parameters
+var devices = [];  // Array of {id, name} objects
+var selectedDeviceId = null;
+var storedParams = [];  // Parameter IDs for selected device
 
 function formatIDarr(idArr){
     var returnArr = [];
@@ -30,6 +37,93 @@ function formatIDarr(idArr){
         }
     });
     return returnArr;
+}
+
+function init(){
+    post('Initializing ctrlGen...\n');
+
+    // Get this device and its parent track/chain
+    var thisDevice = new LiveAPI("this_device");
+    var thisDeviceId = parseInt(thisDevice.id);
+    var parent = new LiveAPI(thisDevice.get("canonical_parent"));
+    post('Parent ID: ' + parent.id + '\n');
+
+    // Get all devices in the parent
+    var devIds = formatIDarr(parent.get("devices"));
+
+    // Clear and rebuild devices array
+    devices = [];
+
+    devIds.forEach(function(devId){
+        // Skip ctrlGen itself
+        if(parseInt(devId) === thisDeviceId){
+            post('Skipping self (ctrlGen)\n');
+            return;
+        }
+
+        var dev = new LiveAPI('id ' + devId);
+        var name = dev.get("name").toString();
+        devices.push({
+            id: devId,
+            name: name
+        });
+        post('Found device: ' + name + ' (ID: ' + devId + ')\n');
+    });
+
+    post('Found ' + devices.length + ' devices\n');
+
+    // Populate the umenu
+    populateDevMenu();
+
+    // Auto-select first device if available
+    if(devices.length > 0){
+        selectDevice(0);
+    }
+}
+
+function populateDevMenu(){
+    var menu = patcher.getnamed("devMenu");
+    if(!menu){
+        post('Error: devMenu umenu not found in patcher\n');
+        return;
+    }
+
+    // Clear the menu
+    menu.message("clear");
+
+    // Add device names to the menu
+    devices.forEach(function(dev){
+        menu.message("append", dev.name);
+    });
+
+    post('Populated devMenu with ' + devices.length + ' devices\n');
+}
+
+function selectDevice(index){
+    if(index < 0 || index >= devices.length){
+        post('Error: Invalid device index ' + index + '\n');
+        return;
+    }
+
+    var device = devices[index];
+    selectedDeviceId = device.id;
+    post('Selected device: ' + device.name + ' (ID: ' + selectedDeviceId + ')\n');
+
+    // Retrieve parameters for this device
+    retrieveParams(selectedDeviceId);
+}
+
+function retrieveParams(devId){
+    var dev = new LiveAPI('id ' + devId);
+    var params = formatIDarr(dev.get("parameters"));
+
+    // Remove first param (device on)
+    params.shift();
+
+    // Store parameters
+    storedParams = params;
+
+    post('Retrieved ' + storedParams.length + ' parameters\n');
 }
 
 function getParamInfo(paramID){
@@ -119,28 +213,35 @@ function createDialWithComment(paramInfo, gridIndex){
     };
 }
 
-function generateUI(deviceId){
+function generateUI(startIndex, stopIndex){
     post('Generating UI...\n');
 
-    if(!deviceId){
-        post('Error: No device ID provided.\n');
+    if(storedParams.length === 0){
+        post('Error: No parameters stored. Select a device first.\n');
         return;
     }
 
     // Clear any previously created objects
     clearCreatedObjects();
 
-    var dev = new LiveAPI('id ' + deviceId);
-    var params = formatIDarr(dev.get("parameters"));
-    // remove first param - dev on
-    params.shift();
+    // Apply start index (default to 0)
+    var start = (typeof startIndex === 'number') ? startIndex : 0;
+    // Apply stop index (default to storedParams.length - 1)
+    var stop = (typeof stopIndex === 'number') ? stopIndex : storedParams.length - 1;
 
-    post('Creating UI for ' + params.length + ' parameters...\n');
+    // Clamp indices to valid range
+    start = Math.max(0, Math.min(start, storedParams.length - 1));
+    stop = Math.max(start, Math.min(stop, storedParams.length - 1));
+
+    // Slice params to the specified range
+    var selectedParams = storedParams.slice(start, stop + 1);
+
+    post('Creating UI for params ' + start + ' to ' + stop + ' (' + selectedParams.length + ' parameters)...\n');
 
     // Store all created element groups for route connections
     var elementGroups = [];
 
-    params.forEach(function(paramId, i) {
+    selectedParams.forEach(function(paramId, i) {
         var paramInfo = getParamInfo(paramId);
         var group = createDialWithComment(paramInfo, i);
         elementGroups.push(group);
@@ -149,14 +250,14 @@ function generateUI(deviceId){
         }
     });
 
-    // Create route object with arguments 0 to (params.length - 1)
+    // Create route object with arguments from start to stop
     var routeArgs = [];
-    for(var i = 0; i < params.length; i++){
+    for(var i = start; i <= stop; i++){
         routeArgs.push(i);
     }
 
     // Position route below all the generated UI
-    var totalRows = Math.ceil(params.length / COLUMNS);
+    var totalRows = Math.ceil(selectedParams.length / COLUMNS);
     var routeY = GRID_START_Y + (totalRows * CELL_HEIGHT) + 20;
     var route = patcher.newdefault.apply(patcher, [GRID_START_X, routeY, "route"].concat(routeArgs));
     createdObjects.push(route);
@@ -166,13 +267,20 @@ function generateUI(deviceId){
         patcher.connect(route, i, group.remote, 1);
     });
 
+    // Output the range
+    outputRange(start, stop);
+
     post('UI generation complete. Created ' + createdObjects.length + ' objects.\n');
+}
+
+function outputRange(start, stop){
+    outlet(0, 'start:', start, 'stop:', stop);
 }
 
 function clear(){
     clearCreatedObjects();
 }
 
-function msg_int(deviceId){
-    generateUI(deviceId);
+function gen(startIndex, stopIndex){
+    generateUI(startIndex, stopIndex);
 }
